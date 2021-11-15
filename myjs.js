0258b99878ca2a61b0f83ed3f951478d7beb0c06
samira talanchi <samira73talanchi@gmail.com>
2021-11-15 13:18:29 +0330

    add number functions

---
+++ myjs.js
@@ -0,0 +1,53 @@
+var value = "";
+var temp = 0
+
+function one() {
+    temp=1;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function two() {
+    temp=2;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function three() {
+    temp=3;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function four() {
+    temp=4;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function five() {
+    temp=5;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function six() {
+    temp=6;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function seven() {
+    temp=7;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function eight() {
+    temp=8;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function nine() {
+    temp=9;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
+function zero() {
+    temp=0;
+    value += temp;
+    document.getElementById("number").innerText = value;
+}
